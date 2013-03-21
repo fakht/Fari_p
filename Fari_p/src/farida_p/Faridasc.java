9a5b4f78d22597e7145bbc91e702ab75d2779c1b
Farida <fakhtar@A01C00490.ad.sogeti.se>
2013-03-21 16:02:59 +0100

    newone

---
+++ Fari_p/src/farida_p/Faridasc.java
@@ -0,0 +1,10 @@
+package farida_p;
+
+public class Faridasc {
+	public static void main (String [] args){
+		int x=7,y=7;
+		boolean comp = x == y;
+		System.out.print(comp);
+
+}
+}
---
+++ Fari_p/src/farida_p/FaridascTest.java
@@ -0,0 +1,17 @@
+package farida_p;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FaridascTest extends Faridasc {
+	
+	
+	@Test
+	public void testMain() {
+		int x=7,y=7;
+		boolean comp = x != y;
+		System.out.print(comp);
+	}
+
+}
